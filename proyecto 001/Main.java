2500265d105cf98fc0977caaf1e518cdf7934b32
milobitodeluna <beatrizruiz.ovb@gmail.com>
2025-02-07 20:32:43 +0100

    ok

--- proyecto 001/Main.java
+++ proyecto 001/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    public static void main(String[] arg) {
-        int a = 10;
-        System.out.println(a);
+    public static void main(String[] args) {
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println("Valor máximo de un int: " + maxValue);
     }
 }
